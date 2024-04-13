1c2710789471939eb68ed71a625a5e2ba7f68c49
Mila <milkakm2003@gmail.com>
2024-04-14 00:26:00 +0200

    Calendar, History and Upcoming Layouts and minor changes

---
+++ app/src/main/java/com/example/madness/adapters/Calendar_Adapter.kt
@@ -0,0 +1,47 @@
+package com.example.madness.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madness.R
+
+class CalendarAdapter(
+    private val daysOfMonth: ArrayList<String>,
+    private val onItemListener: OnItemListener) : RecyclerView.Adapter<CalendarViewHolder>() {
+
+    interface OnItemListener {
+        fun onItemClick(position: Int, dayText: String)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalendarViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_calendar, parent, false)
+        val layoutParams = view.layoutParams
+        layoutParams.height = (parent.height * 0.12).toInt()
+        return CalendarViewHolder(view, onItemListener)
+    }
+
+    override fun onBindViewHolder(holder: CalendarViewHolder, position: Int) {
+        holder.dayOfMonth.text = daysOfMonth[position]
+    }
+
+    override fun getItemCount(): Int {
+        return daysOfMonth.size
+    }
+}
+
+class CalendarViewHolder(itemView: View, private val onItemListener: CalendarAdapter.OnItemListener) :
+    RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+    val dayOfMonth: TextView = itemView.findViewById(R.id.day)
+
+    init {
+        itemView.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        onItemListener.onItemClick(adapterPosition, dayOfMonth.text.toString())
+    }
+}
